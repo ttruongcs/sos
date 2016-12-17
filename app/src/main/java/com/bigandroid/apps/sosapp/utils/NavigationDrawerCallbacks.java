0be291609f964c60cb9525246dc64ca5901ae05a
ttruongcs <tan.truong@banvien.com>
2016-12-17 18:12:46 +0700

    first commit

---
+++ app/src/main/java/com/bigandroid/apps/sosapp/utils/NavigationDrawerCallbacks.java
@@ -0,0 +1,8 @@
+package com.bigandroid.apps.sosapp.utils;
+
+/**
+ * Created by poliveira on 27/10/2014.
+ */
+public interface NavigationDrawerCallbacks {
+    void onNavigationDrawerItemSelected(int position);
+}
